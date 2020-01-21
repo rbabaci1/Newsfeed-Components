d27aed4bd32de5bbbc977cad2c71fa8aaa6c4dc3
rabah babaci <4rabah@gmail.com>
2020-01-20 17:48:45 -0800

    return the menu component

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -38,15 +38,17 @@ function createMenu(menuItems) {
   // append the ul to the menu
   menu.append(ulList);
 
+  // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+  const menuButton = document.querySelector('.menu-button');
+
+  // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  menuButton.addEventListener('click', () => {
+    menu.classList.toggle('menu--open');
+  });
+
+  // Step 5: return the menu component.
+  return menu;
 }
 
-// Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-const menuButton = document.querySelector('.menu-button');
-
-// Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-menuButton.addEventListener('click', () => {
-  menu.classList.toggle('menu--open');
-});
-// Step 5: return the menu component.
 
 // Step 6: add the menu component to the DOM.
