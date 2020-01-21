c1a5fe3b9af56645a149bf42acf81c1422b1bb1d
rabah babaci <4rabah@gmail.com>
2020-01-20 17:45:21 -0800

    select the menu button on the DOM

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -40,9 +40,8 @@ function createMenu(menuItems) {
 
 }
 
-// createMenu(['yes', 'hello']);
-
 // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+const menuButton = document.querySelector('.menu-button');
 
 // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
 
